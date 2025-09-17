9b81ba40cb30b7b3c161e6dcb9bb73ef16dbd4f4
Gao Peter <yu.zheng0622@gmail.com>
2025-09-17 22:56:17 +0800

    牌義增加分隔線

--- JS/CardTypeSelectEvent.js
+++ JS/CardTypeSelectEvent.js
@@ -44,5 +44,4 @@ function GetCardTypeButtonOnclik(){
                    }
             }
         }
-
 }
--- JS/TarotCardDescription.js
+++ JS/TarotCardDescription.js
@@ -4,10 +4,10 @@ function GetTarotCardDescription(cardSelectStatusValue,CardInformation,informati
 	let tarotName = informationJSON[0].Name;
 	if(cardSelectStatusValue == cardStatusEnum.positiveEN)
 	{
-		CardInformation.innerHTML = tarotName + "-" + cardStatusEnum.positiveCN +"：" + informationJSON[0].InformationKeyY+ "<br>";
+		CardInformation.innerHTML = tarotName + "-" + cardStatusEnum.positiveCN +"：" + informationJSON[0].InformationKeyY + "<hr style=\" height:5px;border:none;border-top:2px dotted #0000E3; border-width:5px;\" />";
 	}
 	else
 	{
-		CardInformation.innerHTML = tarotName + "-" + cardStatusEnum.reversedCN +"：" + informationJSON[0].InformationKeyS + "<br>";
+		CardInformation.innerHTML = tarotName + "-" + cardStatusEnum.reversedCN +"：" + informationJSON[0].InformationKeyS + "<hr style=\" height:5px;border:none;border-top:2px dotted #0000E3; border-width:5px;\" />";
 	}
 }
